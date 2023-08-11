4179800ad4403a8b58d48b65d76cf6f18d25a2aa
Keltoum2 <kaltoumyramane23@gmail.com>
2023-08-11 10:36:43 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 52; i++)
