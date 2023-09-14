8bfb72cf0de33e7ffa8c7e629b6b8991b71339e6
Keltoum2 <kaltoumyramane23@gmail.com>
2023-09-14 09:59:12 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,21 +1,9 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef _VARIADIC_H_
+#define _VARIADIC_H_
 
-#include <stdarg.h>
-
-/* Struct definition for print format */
-typedef struct print_format
-{
-	char format;
-	void (*printer)(va_list);
-} 
-print_format_t;
-
-/* Function prototypes */
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
-#endif /* VARIADIC_FUNCTIONS_H */
+#endif
